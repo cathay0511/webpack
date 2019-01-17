8033f4b8b1e736904888515ae3b9f81cbf6c2661
cathayliuyu <cathayliuyu@didichuxing.com>
2019-01-17 15:46:16 +0800

    feat: use elementui

---
+++ template/demo/demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <star-component/>
+  </div>
+</template>
+
+<script>
+
+import StarComponent from '../src/index';
+
+export default {
+  name: 'DemoComponent',
+  components: {
+    StarComponent
+  }
+}
+</script>
--- template/demo/index.js
+++ template/demo/index.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import DemoComponent from '../index'
+import Element from 'element-ui'
+import DemoComponent from './demo'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
